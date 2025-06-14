efb3f5ce525f43a5fa0b6ddc2225f2e2a6d03f7c
ruserxd <zxcmomo0219@gmail.com>
2025-06-15 01:29:26 +0800

    add : CarShop class with car management
    - refactor Car constructor using setName()

--- src/main/java/org/Car.java
+++ src/main/java/org/Car.java
@@ -7,6 +7,16 @@ public class Car {
 
   // Car 的建構子
   public Car(String name) {
+    setName(name);
+  }
+
+  // 獲取 Car 的名稱
+  public String getName() {
+    return this.name;
+  }
+
+  // 設定 Car 的名稱
+  public void setName(String name) {
     this.name = name;
   }
 }
---
+++ src/main/java/org/CarShop.java
@@ -0,0 +1,29 @@
+package org;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Car's Shop 類別
+public class CarShop {
+  // Car's Shop 的名稱
+  String name;
+
+  // Car's Shop 擁有的車子
+  List<Car> carList;
+
+  // Car's Shop 的建構子
+  public CarShop(String name) {
+    this.name = name;
+    this.carList = new ArrayList<>();
+  }
+
+  // 加入 Car 到 CarShop
+  public void addCar(Car car) {
+    carList.add(car);
+  }
+
+  // 獲取 Car's Shop 擁有的 Cars
+  public List<Car> getCarList() {
+    return carList;
+  }
+}
